eff0e6c35d7413724003ec5d40a335ba7dbfd668
Anasmalik57 <itsanas474@gmail.com>
2025-12-01 14:23:04 +0530

    next.config.mjsrollback

--- next.config.mjs
+++ next.config.mjs
@@ -12,19 +12,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://www.nodeskdeveloper.com" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
-          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization" },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
