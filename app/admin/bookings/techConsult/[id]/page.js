49f622a1d118f48244f0cf718e9e67eb08b93973
Anasmalik57 <itsanas474@gmail.com>
2025-12-03 14:08:37 +0530

    [id] page addded

--- app/admin/bookings/techConsult/[id]/page.js
+++ app/admin/bookings/techConsult/[id]/page.js
@@ -75,7 +75,7 @@ export default function AdminTechConsultDetails() {
           <ArrowLeft className="w-5 h-5" /> Back to List
         </button>
 
-        <div className="bg-gradient-to-br from-slate-900/90 to-slate-950/90 border border-cyan-500/30 rounded-2xl p-8 shadow-2xl backdrop-blur-xl">
+        <div className="bg-gradient-to-br  from-slate-900/90 to-slate-950/90 border border-cyan-500/30 rounded-2xl p-8 shadow-2xl backdrop-blur-xl">
           <h1 className="text-3xl font-bold bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent mb-8">
             Consultation Request Details
           </h1>
