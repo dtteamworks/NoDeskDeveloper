5c58ceb46f35e1a141ef6a2dc3e146b2f7134fb0
Anasmalik57 <itsanas474@gmail.com>
2025-12-04 10:35:05 +0530

    devcard minor changes

--- components/Developer/DevCard.js
+++ components/Developer/DevCard.js
@@ -7,6 +7,7 @@ import {
   X,
   Languages,
   Currency,
+  Activity,
 } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -247,12 +248,16 @@ const DevCard = ({ filteredDevelopers }) => {
                   </span>
                 </div>
 
-                {/* Preferred Language */}
+                {/* Available */}
                 <div className="flex flex-col items-center p-2 bg-purple-900/30 rounded-xl border border-purple-500/30">
-                  <IndianRupee className="size-4 text-purple-400 mb-1" />
-                  <span className="text-xs text-purple-300 pb-1">Rate</span>
+                  <Activity className="size-4 text-purple-400 mb-1" />
+                  <span className="text-xs text-purple-300 pb-1">Available</span>
                   <span className="text-sm font-bold text-white">
-                    {dev?.hourlyRate}
+                   {dev?.available == true ? (
+                        "Yes"
+                      ) : (
+                        "No"
+                      )}
                   </span>
                 </div>
               </div>
