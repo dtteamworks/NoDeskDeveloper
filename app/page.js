97efe5c420cc2ecaf2427a26a4540ddbe91339b9
Anasmalik57 <itsanas474@gmail.com>
2025-12-09 14:50:21 +0530

    minor changeg

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
-"use client";
-import useINRConverter from "@/utils/currencyConverter";
 import HomeComponent from "@/components/Home/HomeComponent";
 import TestimonialsComponent from "@/components/Testimonials/TestimonialsComponent";
 
 const Home = () => {
-  const { convertINR, loading } = useINRConverter();
   return (
     <>
-      
-      <div className="text-center text-4xl font-bold">
-        Price: {loading ? "Loading price..." : convertINR(999)}
-      </div>
-     
       <HomeComponent />
     </>
   );
